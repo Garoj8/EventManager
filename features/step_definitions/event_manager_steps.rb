72ac8e18728eec290f0cfdd6ec931d9abb456fb5
Garoj8 <Garoj8@gmail.com>
2014-05-07 22:08:45 -1000

    inital commit

--- features/step_definitions/event_manager_steps.rb
+++ features/step_definitions/event_manager_steps.rb
@@ -1,6 +1,6 @@
 When(/^I go to the Role\-Sign\-up page$/) do
   step 'I am on the home page'
-  step 'I should see "Event Manager"'
+  step 'I should see "HPU Toastmasters prototype"'
   step 'I click on "Role Sign Up"'
 end
 
